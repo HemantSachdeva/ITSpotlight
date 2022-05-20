51d4f8af467b360347f2ec4d94b5786ce715dc0b
Hemant Sachdeva <hemant.evolver@gmail.com>
2022-05-20 07:30:03 +0530

    Preparing for Production
    Signed-off-by: Hemant Sachdeva <hemant.evolver@gmail.com>

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ CORS(app)
 @app.route('/')
 @app.route('/index')
 def index():
-    resp = requests.get('http://127.0.0.1:5000/api/items/page/1')
+    resp = requests.get('http://itspotlight.herokuapp.com/api/items/page/1')
     latest_news = resp.json()
     context = {
         "latest_news": latest_news
